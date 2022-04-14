a1d390c904a95ebdfc73c6e1afc9be17003d615c
Catherine Riopel <criopel@sfu.ca>
2022-04-13 18:34:41 -0700

    Change argument for set speed function

--- led.h
+++ led.h
@@ -5,7 +5,7 @@
 #define _LED_H_
 
 void Led_init(void);
-void Led_setSpeed(const char *newMessage);
+void Led_setSpeed(const char *newDirection);
 void Led_notifyOnTimeIsr(void);
 void Led_doBackgroundWork(void);
 
