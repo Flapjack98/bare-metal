25a231d7f0943b9b5b4aa2e972e38502c2076849
Catherine Riopel <criopel@sfu.ca>
2022-04-13 21:56:56 -0700

    Start clear reset event bit

--- resetSource.c
+++ resetSource.c
@@ -6,14 +6,13 @@
 #include "wdtimer.h"
 
 /*****************************************************************************
- **                Internal Macro Definitions
- *****************************************************************************/
-#define RESET_SOURCE_REG_ADDR (0x4030CE4C)
-#define RESET_SOURCE_OFFSET (0x8)
-
-/*****************************************************************************
  **                Static Constants
  *****************************************************************************/
+static const unsigned int REGISTERS_BASE_ADDRESS = (0x44E00F00);
+static const unsigned int RESET_SOURCE_OFFSET = (0x8);
+static const unsigned int RESET_SOURCE_REGISTER_ADDRESS = 
+(REGISTERS_BASE_ADDRESS + RESET_SOURCE_OFFSET);
+
 static const int MAX_BUFFER_SIZE = 1024;
 static const int NUMBER_RESET_EVENT_TYPES = 5;
 
@@ -31,6 +30,11 @@ static ResetEvent ResetEventInfo[] = {
     {0, "Powen-on (cold)"}
 }
 
+static void ResetSource_clearResetEventBit(int resetEventBit)
+{
+    
+}
+
 static char *ResetSource_readFromResetSource(void)
 {
     char resetSourceListString[MAX_BUFFER_SIZE];
@@ -38,7 +42,7 @@ static char *ResetSource_readFromResetSource(void)
         // Get the pin
         int resetEventMask = (1<<ResetEventInfo[i].bit);
         // Apply the mask
-        int result = HWREG(RESET_SOURCE_REG_ADDR + RESET_SOURCE_OFFSET) |= resetEventMask;
+        int result = HWREG(REGISTERS_BASE_ADDRESS + RESET_SOURCE_OFFSET) |= resetEventMask;
         // Check to see if bit is 1
         if (result) {
             // Make a string of of the resets found
@@ -46,6 +50,7 @@ static char *ResetSource_readFromResetSource(void)
             strcat(resetSourceListString, source);
             strcat(resetSourceListString, ", ");
         }
+        ResetSource_clearResetEventBit(ResetEventInfo[i].bit);
     }
 
     return resetSourceListString;
